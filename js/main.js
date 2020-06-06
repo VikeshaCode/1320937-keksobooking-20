897d59cd4d7d243e2e0a033c0918bf2bd5980d51
VikeshaCode <Vikeshadoc@gmail.com>
2020-06-07 00:21:32 +0300

    Add pin-img atributes

--- js/main.js
+++ js/main.js
@@ -20,19 +20,18 @@ var generateObject = function (x) {
       checkin: time[Math.round(Math.random() * 2)],
       checkout: time[Math.round(Math.random() * 2)],
       features: features.slice(
-          Math.round(Math.random() * features.length),
-          Math.round(Math.random() * features.length)),
+        Math.round(Math.random() * features.length),
+        Math.round(Math.random() * features.length)),
       description: 'Oписание ' + x,
       photos: photos.slice(
-          Math.round(Math.random() * photos.length),
-          Math.round(Math.random() * photos.length)),
+        Math.round(Math.random() * photos.length),
+        Math.round(Math.random() * photos.length)),
     },
     location: {
       x: Math.round(Math.random() * map.getBoundingClientRect().width),
       y: Math.round(Math.random() * 500) + 130,
     }
   };
-
   object.offer.address = object.location.x + ', ' + object.location.y;
   return object;
 };
@@ -49,6 +48,7 @@ var generateArray = function () {
 var data = generateArray();
 
 var pins = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
 var pin = document.querySelector('#pin')
   .content
   .querySelector('button');
@@ -58,6 +58,12 @@ for (var i = 0; i < data.length; i++) {
   var element = pin.cloneNode(true);
   element.style.left = currentDatum.location.x + (pin.getBoundingClientRect().width / 2) + 'px';
   element.style.top = currentDatum.location.y + (pin.getBoundingClientRect().height / 2) + 'px';
-  pins.appendChild(element);
+
+  var picture = element.firstElementChild;
+  picture.src = currentDatum.author.avatar;
+  picture.alt = currentDatum.offer.title;
+  fragment.appendChild(element);
 }
+pins.appendChild(fragment);
+
 
